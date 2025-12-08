78a1af860ca360470fc3239a980c17fb4095b58e
sojsnake <sojsnake@naver.com>
2025-12-08 13:45:04 +0900

    refactor: response 수정 #9

--- src/main/java/com/tickatch/reservationservice/reservation/application/dto/ReservationResponse.java
+++ src/main/java/com/tickatch/reservationservice/reservation/application/dto/ReservationResponse.java
@@ -1,10 +1,12 @@
 package com.tickatch.reservationservice.reservation.application.dto;
 
 import com.tickatch.reservationservice.reservation.domain.Reservation;
+import com.tickatch.reservationservice.reservation.domain.ReservationStatus;
 import java.util.UUID;
 
 public record ReservationResponse(
-    UUID id, UUID reserverId, long productId, long seatId, Long price) {
+    UUID id, UUID reserverId, long productId, long seatId, Long price, ReservationStatus status,
+    String reservationNumber) {
 
   public static ReservationResponse from(Reservation reservation) {
     return new ReservationResponse(
@@ -12,6 +14,8 @@ public record ReservationResponse(
         reservation.getReserver().getId(),
         reservation.getProductInfo().getProductId(),
         reservation.getProductInfo().getSeatId(),
-        reservation.getProductInfo().getPrice());
+        reservation.getProductInfo().getPrice(),
+        reservation.getStatus(),
+        reservation.getReservationNumber());
   }
 }
